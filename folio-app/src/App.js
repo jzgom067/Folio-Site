b6b7b02120753a2587180c29284e4447d6fbaf47
honketh <69125339+jzgom067@users.noreply.github.com>
2024-05-07 19:54:52 -0400

    Change page metadata and icon
    Also changed one link on the page, but I'm still figuring out how everything works.

--- folio-app/src/App.js
+++ folio-app/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jzgombic.itch.io/golf"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Golf
         </a>
       </header>
     </div>
