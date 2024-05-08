e15fb97272f80000dae1b687e885313dafb7463d
honketh <jzgombic3@gmail.com>
2024-05-08 15:52:56 -0400

    Add grid layout, header and footer
    The site itself is super ugly, but I'm starting to get an idea of the layout I want.

--- folio-app/src/App.js
+++ folio-app/src/App.js
@@ -1,23 +1,67 @@
 import banana from './banan.png';
 import './App.css';
 
+const ProjGrid = ({ items }) => {
+  return (
+    <div className="projects">
+      {items.map((item, index) => (
+        <div key={index} className="project">
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+          <a
+            className="link"
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer external"
+          >
+            {item.link_name}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const items = [
+  {
+    name: 'Super Golf',
+    description: "A golf game.",
+    link: 'https://jzgombic.itch.io/golf',
+    link_name: 'itch.io'
+  },
+  {
+    name: 'CARPI Bot',
+    description: "A Discord bot tailored for RPI students.",
+    link: 'https://github.com/SameriteRL/CARPI-Bot',
+    link_name: 'GitHub'
+  },
+  {
+    name: 'This Website...?',
+    description: "Not much here at the moment.",
+    link: 'https://github.com/jzgom067/Folio-Site',
+    link_name: 'GitHub'
+  },
+  {
+    name: 'More to come...',
+    description: "",
+    link: '',
+    link_name: ''
+  }
+]
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={banana} className="App-logo" alt="logo" />
-        <p className="App-text">
+    <div className="main">
+      <header className="header">
+        <img src={banana} className="logo" alt="logo" />
+        <p className="text">
           👋
         </p>
-        <a
-          className="App-link"
-          href="https://jzgombic.itch.io/golf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Golf
-        </a>
       </header>
+      <ProjGrid items={items} />
+      <footer className = "footer">
+        footer
+      </footer>
     </div>
   );
 }
