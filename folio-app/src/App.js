23c10cb7c05882c853f3913b2c362cbd7ea5da00
honketh <jzgombic3@gmail.com>
2024-05-14 01:35:10 -0400

    Change header format, add elements
    Header padding was removed, so each element can define its own and be able to stretch from top to bottom.
    The favicon was finally changed to one I made myself.
    The logo in the header was also removed, and a link to my LinkedIn was added.

--- folio-app/src/App.js
+++ folio-app/src/App.js
@@ -1,17 +1,33 @@
-import banana from './img/banan.png';
 import projects from './modules/projects.js';
 import ProjGrid from './modules/ProjGrid.js';
-// import DevIcon from './modules/DevIcon.js';
+import ExtLink from './modules/ExtLink.js';
 import './App.css';
 
 function App() {
   return (
     <div className="main">
       <header>
-        <img src={banana} className="logo" alt="logo" />
         <div className="name">
           Jack Zgombic
         </div>
+        <a
+          className="profile-link"
+          href="https://github.com/jzgom067"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>GitHub</span>
+          <ExtLink></ExtLink>
+        </a>
+        <a
+          className="profile-link"
+          href="https://linkedin.com/in/jack-zgombic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>LinkedIn</span>
+          <ExtLink></ExtLink>
+        </a>
       </header>
       <h1 className="section-title">Projects</h1>
       <ProjGrid projects={projects} />
