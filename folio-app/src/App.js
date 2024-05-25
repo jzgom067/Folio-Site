2b4ff434696d0f91015df633f5933866bcc84952
honketh <jzgombic3@gmail.com>
2024-05-24 20:44:59 -0400

    Reset fade in delay on page change
    Fade in delay didn't reset when going to other pages. This means you would have to wait a while after going back to the home page for things to show up. Now any time you go to a new page, the delay is reset.

--- folio-app/src/App.js
+++ folio-app/src/App.js
@@ -1,10 +1,26 @@
+// react
+import React from 'react';
+
+// data
+import globals from './data/globals.js';
+
+// components
 import NavBar from './components/NavBar.js';
 import Content from './components/Content.js';
+
+// router
+import { useLocation } from 'react-router-dom';
 
 // styling
 import './App.css';
 
 function App() {
+  let location = useLocation();
+
+  React.useEffect(() => {
+    globals.fadeInDelay = 0;
+  }, [location]);
+
   return (
     <div className="main">
       <NavBar />
