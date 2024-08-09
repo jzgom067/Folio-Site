33e804ae566a7e32b2abe3fcaa71c291ef206463
jzgom067 <jzgombic3@gmail.com>
2024-08-09 16:53:25 -0400

    Add image switching
    With the press of a button, the image magically swaps out for the next one.

--- folio-app/src/components/ImageModal.js
+++ folio-app/src/components/ImageModal.js
@@ -9,21 +9,35 @@ import styles from './ImageModal.module.css';
 
 function ImageModal({ images }) {
   const [modalOpen, setOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  function nextImage() {
+    setCurrentImage(currentImage + 1);
+  }
+
+  function prevImage() {
+    setCurrentImage(currentImage - 1);
+  }
 
   return (
     <div
       className={styles.modalTrigger}
-      onMouseDown={() =>
-        setOpen(true)
-      }
+      onMouseDown={() => {
+        if (!modalOpen) {
+          setOpen(true);
+          setCurrentImage(0);
+        }
+      }}
     >
       <Modal
         isOpen={modalOpen}
         className={styles.modalBody}
         overlayClassName={styles.modalOverlay}
       >
-        <img className={styles.image} src={images[0]} alt="" />
+        <img className={styles.image} src={images[currentImage]} alt="" />
         <button onClick={() => setOpen(false)}>Close Modal</button>
+        <button onClick={prevImage} disabled={currentImage === 0}>Previous</button>
+        <button onClick={nextImage} disabled={currentImage === images.length - 1}>Next</button>
       </Modal>
     </div>
   );
