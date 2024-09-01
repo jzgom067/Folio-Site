c9617880a83c5782f7bfa76d80661c3fc1808482
jzgom067 <jzgombic3@gmail.com>
2024-08-31 22:06:11 -0400

    Clean up AboutMe
    Make my life easier, avoid rewriting so much code.

--- folio-app/src/components/AboutMe.js
+++ folio-app/src/components/AboutMe.js
@@ -4,11 +4,17 @@ import SlideFadeIn from './SlideFadeIn.js';
 // styling
 import styles from './AboutMe.module.css';
 
+var text = [
+  "Hi! I'm Jack Zgombic, a current student at RPI that's pursuing a degree in Computer Science.",
+];
+
 const AboutMe = () => (
   <div className={styles.row}>
-    <p>
-      Hi! I'm Jack Zgombic, a current student at RPI that's pursuing a degree in Computer Science.
-    </p>
+    {text.map((content, i) => (
+      <SlideFadeIn key={i}>
+        <p>{content}</p>
+      </SlideFadeIn>
+    ))}
   </div>
 );
 
