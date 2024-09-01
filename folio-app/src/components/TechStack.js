b131baa6bcff9db0a5e535d9933fb3acd1b5a748
jzgom067 <jzgombic3@gmail.com>
2024-08-31 21:00:53 -0400

    Change TechStack to be more modular
    The code is a LOT less ugly, with no more manual z index definition.

--- folio-app/src/components/TechStack.js
+++ folio-app/src/components/TechStack.js
@@ -5,80 +5,65 @@ import SlideFadeIn from './SlideFadeIn.js';
 // styling
 import styles from './TechStack.module.css';
 
-var stack = {
-  lang: [
-    "Python",
-    "C#",
-    "Java",
-    "MySQL",
-    "C",
-    "C++",
-    "Looker/LookML",
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "Dart",
-  ],
-  frames: [
-    "Alpine.js",
-    "Tailwind CSS",
-    "React",
-    "Node.js",
-    "Flutter",
-  ],
-  tools: [
-    "Visual Studio Code",
-    "Unity",
-    "Blender",
-    "Firebase",
-  ]
-}
+var stack = [
+  {
+    name: "Languages",
+    tech: [
+      "Python",
+      "C#",
+      "Java",
+      "MySQL",
+      "C",
+      "C++",
+      "Looker/LookML",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Dart",
+    ],
+  },
+  {
+    name: "Libraries/Frameworks",
+    tech: [
+      "Alpine.js",
+      "Tailwind CSS",
+      "React",
+      "Node.js",
+      "Flutter",
+    ],
+  },
+  {
+    name: "Tools/Software",
+    tech: [
+      "Visual Studio Code",
+      "Unity",
+      "Blender",
+      "Firebase",
+    ]
+  }
+];
 
 const TechStack = () => {
   return (
     <div className={styles.stack}>
-      <div style={{ zIndex: "5" }}>
-        <SlideFadeIn>
-          <h2>Languages</h2>
-        </SlideFadeIn>
-      </div>
-      <div style={{ zIndex: "4" }}>
-        <SlideFadeIn>
-          <div className={styles.icons}>
-            {stack.lang.map((tech, index) => (
-              <DevIcon tech={tech} size="4rem" key={index} />
-            ))}
+      {stack.map((group, i) => (
+        <>
+          <div style={{ zIndex: `${stack.length - i}` }}>
+            <SlideFadeIn>
+              <h2>{group.name}</h2>
+            </SlideFadeIn>
           </div>
-        </SlideFadeIn>
-      </div>
-      <div style={{ zIndex: "3" }}>
-        <SlideFadeIn>
-          <h2>Libraries/Frameworks</h2>
-        </SlideFadeIn>
-      </div>
-      <div style={{ zIndex: "2" }}>
-        <SlideFadeIn>
-          <div className={styles.icons}>
-            {stack.frames.map((tech, index) => (
-              <DevIcon tech={tech} size="4rem" key={index} />
-            ))}
+          <div style={{ zIndex: `${stack.length - i}` }}>
+            <SlideFadeIn>
+              <div className={styles.icons}>
+                {group.tech.map((tech, index) => (
+                  <DevIcon tech={tech} size="4rem" key={index} />
+                ))}
+              </div>
+            </SlideFadeIn>
           </div>
-        </SlideFadeIn>
-      </div>
-      <div style={{ zIndex: "1" }}>
-        <SlideFadeIn>
-          <h2>Tools/Software</h2>
-        </SlideFadeIn>
-      </div>
-      <div style={{ zIndex: "0" }}>
-        <SlideFadeIn>
-          <div className={styles.icons}>
-            {stack.tools.map((tech, index) => (
-              <DevIcon tech={tech} size="4rem" key={index} />
-            ))}
-          </div>
-        </SlideFadeIn>
-      </div>
+        </>
+      ))}
     </div>
   );
 }
