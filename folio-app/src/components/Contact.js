55e08eb1cde191a0a96ce1e4474c97101aee21a8
jzgom067 <jzgombic3@gmail.com>
2024-09-08 23:15:03 -0400

    Add row layout to contact
    Slowly getting to where I want.

--- folio-app/src/components/Contact.js
+++ folio-app/src/components/Contact.js
@@ -6,40 +6,44 @@ import styles from './Contact.module.css';
 
 const Contact = () => (
   <div className={styles.contact}>
-    <SlideFadeIn>
-      <h2 className={styles.subtitle}>Looking for my resume?</h2>
-    </SlideFadeIn>
-    <SlideFadeIn>
-      <a
-        className={styles.resume}
-        href={process.env.PUBLIC_URL + "/resume.pdf"}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Here's a copy
-      </a>
-    </SlideFadeIn>
-    <SlideFadeIn>
-      <h2>What about my email?</h2>
-    </SlideFadeIn>
-    <SlideFadeIn>
-      <a
-        className={styles.resume}
-        href="mailto:jzgombic7@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Send me one here
-      </a>
-    </SlideFadeIn>
-    <SlideFadeIn>
-      <div
-        className={styles.resume}
-        onClick={() => navigator.clipboard.writeText('jzgombic7@gmail.com')}
-      >
-        (Or copy the address)
-      </div>
-    </SlideFadeIn>
+    <div className={styles.row}>
+      <SlideFadeIn>
+        <h2>Looking for my resume?</h2>
+      </SlideFadeIn>
+      <SlideFadeIn>
+        <a
+          className={styles.resume}
+          href={process.env.PUBLIC_URL + "/resume.pdf"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Here's a copy
+        </a>
+      </SlideFadeIn>
+    </div>
+    <div className={styles.row}>
+      <SlideFadeIn>
+        <h2>What about my email?</h2>
+      </SlideFadeIn>
+      <SlideFadeIn>
+        <a
+          className={styles.resume}
+          href="mailto:jzgombic7@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Send me one here
+        </a>
+      </SlideFadeIn>
+      <SlideFadeIn>
+        <div
+          className={styles.resume}
+          onClick={() => navigator.clipboard.writeText('jzgombic7@gmail.com')}
+        >
+          (Or copy the address)
+        </div>
+      </SlideFadeIn>
+    </div>
   </div>
 );
 
