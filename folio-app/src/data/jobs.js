bf463e66eb9f25a3dea6ce3938101717d7881ee6
jzgom067 <jzgombic3@gmail.com>
2024-10-14 02:02:18 -0400

    Edit project and experience text
    I focused on not only fixing flow and grammar, but trying to shorten the amount of text as much as possible.
    I've been told many times that nobody is going to read it, and I wholeheartedly agree.

--- folio-app/src/data/jobs.js
+++ folio-app/src/data/jobs.js
@@ -25,23 +25,23 @@ const jobs = [
     text: [
       {
         title: "What is Hiveclass?",
-        text: "Hiveclass is a digital physical education platform that reimagines youth sports. It solves a problem that largely came about during the pandemic—keeping students active while quarantined at home. With the abundance of content available on the internet today, there was a surprising lack of engaging content when it came to sports. Hiveclass takes advantage of technology's evolution to offer high-quality physical education at your fingertips."
+        text: "Hiveclass is a digital physical education platform that is \"reimagining youth sports\". It solves a problem that largely came about during the pandemic—keeping students active while remote. With the abundance of content available on the internet, there is a surprising lack of high-quality and engaging content when it comes to sports. Hiveclass aims to fill that gap and more with a platform that provides videos and lessons across various sports and activities."
       },
       {
         title: "What have I contributed?",
-        text: "Since joining the team in June 2023, I've been almost exclusively on the backend side of the platform. My role involves working with the database and generating analytics for the internal team. At one point, I tried out some full-stack development to make a tool that makes that information more accessible. After that, I dove head-first into learning how to use Looker for a more interactive and extensive platform for data visualization. Since then, I've been working to maintain those dashboards, adding more and more calculations for advanced analytics."
+        text: "Since joining the team in June 2023, I've been almost exclusively on the backend side of the platform. My role involves working with the database and generating reports for the internal team. I made a tool that makes that information accessible via a website, but I quickly dove head-first into learning how to use Looker for a more extensive and interactive data visualization platform. Since then, I've been working to maintain dashboards, adding more and more calculations for advanced analytics."
       },
       {
         title: "What have I learned?",
-        text: "When starting this role, I very quickly learned about the fast pace of startups. At a moments notice I would be switching gears and working on something completely new, which usually involved learning a new system. For example, I had no experience working with databases and SQL coming into this role. Learning on the job can be a bit stressful, but I've come to appreciate the opportunity. The same goes for Looker, which leverages my SQL experience, but is unlike any system I've worked with before."
+        text: "After starting this role, I quickly learned about the fast pace of startups. At a moments notice I would be switching gears and working on something completely new, which usually involved learning some new system. Learning on the job can be stressful, but I've come to appreciate the opportunity, as I've gained many new skills that I wouldn't have otherwise."
       },
       {
         title: "What challenges have I faced?",
-        text: "Given that Hiveclass is a startup with limited resources, I was often told to figure out how to do everything on my own. I was free to use anything I wanted to accomplish the goals, as long as it got done. It's a very stark departure from what I imagined would happen in a professional setting, but I've come to appreciate the freedom and trust that comes with it. I think that being able to teach yourself skills is invaluable, and the opportunity to do so in an environment like this is one of a kind."
+        text: "Given that Hiveclass is a startup with limited resources, I was often told to figure out how to do everything on my own. I was free to use anything I wanted to accomplish my tasks, as long as it got done. It's not exactly what I imagined for a corporate job, but I've come to appreciate the freedom and trust that comes with it. I think that being able to teach yourself skills is invaluable, and the opportunity to do so in an environment like this is one of a kind."
       },
       {
         title: "What's next?",
-        text: "At this point, I've had experience working with Hiveclass's backend and database for over a year. While I'm probably not allowed to talk about the company's plans for the future, I can say that I'm excited to see where my expertise could be used elsewhere. I've worked on a variety of projects, and I'm proud of all the things I've accomplished thus far."
+        text: "At this point, I've had experience working with Hiveclass's backend and database for over a year. While I'm probably not allowed to talk about the company's future plans, I'm excited to see where my expertise could be used elsewhere. I've worked on a variety of projects for the company, and I'm proud of the things I've done for them."
       }
     ]
   }
--- folio-app/src/data/projects.js
+++ folio-app/src/data/projects.js
@@ -72,19 +72,19 @@ const projects = [
     text: [
       {
         title: "What is ChatKeyPT?",
-        text: "ChatKeyPT is a platform that takes a user-supplied OpenAI API key and provides a familiar interface to take advantage of it. The idea behind this project was to make the API's pay-as-you-go model available to a wider audience, giving access to features and models gated behind ChatGPT Plus's hefty monthly fee."
+        text: "ChatKeyPT is a platform that takes a user-supplied OpenAI API key and provides a familiar interface to take advantage of it. The idea behind this project is to make the API's pay-as-you-go model available to a wider audience, giving access to features and models gated behind ChatGPT Plus's hefty monthly fee."
       },
       {
         title: "What did I contribute?",
-        text: "I worked on this project with a friend of mine, who approached me with the idea in May 2024. When it came to splitting the work, he took charge of the backend, I handled the API calls and internal app logic, and we split the rest of the frontend development between the two of us. Of that, I was responsible for the chat page, which involved laying out most of the app's data and planning ahead to make sure the app was scalable."
+        text: "I worked on this project with a friend of mine, who approached me with the idea in May 2024. He took charge of the backend, I handled the API calls and internal app logic, and we split the rest of the frontend development between the two of us. Of that, I was responsible for the chat page, which involved laying out most of the app's data and planning ahead to make sure the app was scalable."
       },
       {
         title: "What did I learn?",
-        text: "This project introduced me to Flutter, which is a stark departure from the React web development I was doing on my personal website. I found that while it took some time to adjust, there's a lot of conveniences that come with the framework, like an automatic formatter, strict code linter, and built-in cross platform support. As part of this project, we planned on releasing as both a website and mobile app. While unfortunately the Android version never made it to production, the iOS version was my responsibility. I learned a lot from not just testing on mobile, but also the process of submitting an app to the App Store."
+        text: "This project introduced me to Flutter, a stark departure from the React I was using for my personal website at the time. It took some time to adjust, but there's a lot of conveniences built-in, like the automatic formatter, strict code linter, and cross-platform support. We planned on releasing both a website and mobile app from the very beginning, and the iOS version was my responsibility. I learned a lot from developing on mobile for the first time, and also from the process of submitting an app to the App Store."
       },
       {
         title: "What challenges did I face?",
-        text: "As said before, the app was released as both a website and mobile app. I find that it's already hard enough making a reactive interface for a website on desktop. During testing, I had to run the same app on a browser, my iPhone, and an Android emulator all at the same time to make sure everything was working smoothly. I also learned a lot about the Flutter community, and how many (seemingly simple) functions of the framework depend on packages maintained by third parties. It was nice to find packages that could accomplish niche things, but typically the documentation was lacking, and many problems took a lot more time than they probably should have to solve."
+        text: "I find that it's hard enough making a reactive and accessible interface for a website. To do the same for a mobile app alongside it was a tall task, especially considering I've never developed on mobile before. I also learned a lot about the Flutter community, and how many (seemingly simple) functions depend on packages maintained by third parties. It was convenient to find packages that could accomplish niche things, but typically the documentation was lacking."
       },
       {
         title: "What's next?",
@@ -117,23 +117,23 @@ const projects = [
     text: [
       {
         title: "What is this website?",
-        text: "This is a website made to be a portfolio for my projects, but also to introduce myself to web development. I've been told many times that a personal website is a perfect way for a software developer to show off their work, and I finally decided to spend a summer creating one. This was also an opportunity for me to reflect on everything I've done, taking a step back and being proud of how far I've come."
+        text: "This website is mainly a project portfolio, but it was also a way to introduce myself to web development. I've been told countless times that a personal website is the perfect way to show off coding projects, and I finally decided to spend a summer creating one. This was also an opportunity for me to reflect on everything I've done, taking a step back and seeing how far I've come."
       },
       {
         title: "What did I contribute?",
-        text: "This was another solo project, so I was responsible for everything between design and implementation. This project was done from scratch, and any problems I had required detailed research to solve. Without someone guiding me, I feel like I properly learned how things worked through plenty of experimenting. Once I had a good grasp of web development's (and my own) capabilitites, I started figuring out my plans for the website."
+        text: "I was responsible for everything between design and implementation, so any problems I had required detailed research to solve. Though, without someone constantly giving me the answer, I feel like I learned well through plenty of experimenting. I started figuring out plans for the website once I had a good grasp of my capabilities."
       },
       {
         title: "What did I learn?",
-        text: "Before this, the last time I worked with HTML was sometime in early high school. I don't remember much from that experience, but since then I've learned a lot about the web development process, in terms of frameworks and development methodologies. I decided to use React for its popularity and extensive documentation, but I still had to completely reteach myself how to use HTML and CSS, which are fundamentally different from languages I typically use. It took a bit to wrap my mind around initially, but most things were relatively intuitive, save for a few confusing CSS properties. Deployment was handled by Vercel, which abstracted much of those complications away from me, allowing me to focus on building the website itself."
+        text: "Before this, the last time I used HTML was in early high school. Since then I've learned about web development frameworks, so I decided to use React for its popularity and extensive documentation. I still had to relearn HTML and CSS, which are fundamentally different from languages I typically use, but things were relatively intuitive save for some CSS properties. Deployment is handled by Vercel, which abstracted those complications away from me so I could focus on building the website."
       },
       {
         title: "What challenges did I face?",
-        text: "I decided to dive right into this project without trying to learn fundamentals first. I've found that I learn very well with hands-on experience, and I wanted to have something presentable by the end of the summer. On top of designing the website, I had to learn 3 different languages at once, that being HTML, CSS, and JavaScript. I feel like React's concepts were very intuitive, making easily reusable components, and it definitely simplified the entire process. However, I think what gave me the most trouble was accounting for mobile users and accessibility settings. There's a delicate balance that needs to be struck between creative expression and making sure the website is accessible."
+        text: "I dove right into this project without trying to learn fundamentals first. I find that I learn very well with hands-on experience, and I wanted to have something presentable by the end of the summer. On top of designing the website, I had to learn HTML, CSS, JavaScript, and React all at once. However, I think what gave me the most trouble was accounting for mobile users and accessibility settings. There's a delicate balance that needs to be struck between creative expression and making sure the website is accessible."
       },
       {
         title: "What's next?",
-        text: "While this website definitely isn't perfect, I'm quite satisfied with the result, considering I've never done web development before. I don't think I'll be making another personal website anytime soon, but I'll keep this updated with any future projects and experience."
+        text: "While this website isn't perfect, I'm quite satisfied with the result, considering it's the first I've made. I'm not planning on making another personal website anytime soon, but I'll keep this updated with future projects and experience."
       }
     ]
   },
@@ -188,23 +188,23 @@ const projects = [
     text: [
       {
         title: "What is CARPI Bot?",
-        text: "CARPI Bot is a Discord bot designed for use by students at Rensselaer Polytechnic Institute. There, Discord is tightly integrated with the community, so the bot is a helpful resource on a platform that people are already on. It features stores of information like the academic calendar and course catalog, with more features and improvements currently being developed. This project is part of the Rensselaer Center for Open Source (RCOS) program, which focuses on the development of open source solutions for real world problems."
+        text: "CARPI Bot is a Discord bot designed for use by students at Rensselaer Polytechnic Institute. Discord is tightly integrated with the community there, so the bot is a helpful resource on a platform that people are already on. It currently features information like the academic calendar and course catalog. This project is part of the Rensselaer Center for Open Source (RCOS) program, which focuses on the development of open source solutions for real world problems."
       },
       {
         title: "What did I contribute?",
-        text: "I started contributing to this project in December 2023, creating a proof of concept for migrating existing data to a MySQL database. I officially joined the project in January 2024, converting already-developed features to use a database as a more efficient solution for data storage. Initially, my time was spent designing the database that would be the foundation for the entire bot from that point forward. After that, I took charge of development on the Course Search feature, which I rewrote from the ground up to use our database instead of scraping data from the unreliable catalog website. This feature was pushed to production in April 2024."
+        text: "My first contribution was a proof of concept for migrating existing data to a more efficient storage solution using MySQL. I officially joined the project in January 2024, where my first task was to design a database. Afterwards, I took charge of development on the Course Search feature, where I leveraged the database for a new search algorithm. This feature was pushed to production in April 2024."
       },
       {
         title: "What did I learn?",
-        text: "This is my first project that involved working with a team and using GitHub for version control. There was a lot to learn in addition to refreshing my Python knowledge from years ago. I was also working with many new libraries, so a lot of adapting was necessary to contribute to this project. Everything in our development process moved quickly, but I had a lot of support from other team members to keep me on track."
+        text: "This is my first project that involved working with a team and using GitHub for version control. In addition to refreshing my Python knowledge from years ago, I was also working with many new libraries, so this project tested my ability to adapt on the fly. Everything in our development process moved quickly, but I had plenty of support from other team members to keep me on track."
       },
       {
         title: "What challenges did I face?",
-        text: "The biggest challenge in this project is managing many different interacting components, like interfacing with databases, implementing external libraries, and using Discord's API. We often had to decipher whether a problem was a bug from a library, or a mistake on our part. We've had to switch out libraries on a few occasions to solve issues like this."
+        text: "The biggest challenge in this project is easily the many interacting components. The bot interfaces with databases, implements external libraries, and uses Discord's API. We've often had to decipher whether a problem was a library bug or a mistake on our part, and we've even switched libraries to solve issues like this."
       },
       {
         title: "What's next?",
-        text: "I plan to continue working on this project in future semesters during my time at RPI. I would like to iterate more on the Course Search, as well as continue to manage the database to include future features of the bot. I look forward to seeing the potential for this bot in the community."
+        text: "I plan to continue working on this project in future semesters during my time at RPI. In particular, I would like to iterate more on the Course Search and keep the database updated to include future features of the bot. I look forward to seeing the potential for this bot in the community."
       }
     ]
   },
@@ -264,7 +264,7 @@ const projects = [
     text: [
       {
         title: "What is Super Golf?",
-        text: "Super Golf is a 3D multiplayer golf game, made in the Unity engine. It features a set of 3 abilities that manipulate the trajectory of the ball while midair, which encourages creativity and strategic planning before each shot. Development time for the first release spanned 2 months, and the game was updated about a year later to fix bugs and add minor features."
+        text: "Super Golf is a 3D multiplayer golf game made in the Unity engine. The main feature is a set of 3 abilities that manipulate the trajectory of the ball while midair, encouraging creativity and strategic planning before each shot. The first release was developed over 2 months, and the game was updated about a year later to fix bugs and add minor features."
       },
       {
         title: "What did I contribute?",
@@ -272,15 +272,15 @@ const projects = [
       },
       {
         title: "What did I learn?",
-        text: "At the point of starting this project, I had already been working with Unity for a few years. However, I had only made small prototypes and proofs of concepts, which amounted to nothing substantial. This project challenged me to commit myself to a game and see it through to the end. Because of this, I learned a lot of concepts that only come up when polishing a game for release. For example, designing a user interface was new to me, and it was a challenge figuring out how to effectively communicate information to the player. Playtesting was integral to getting things right, and it made sure I wasn't completely insane when making the game. There were also challenges that arose from the type of game I was making. For example, multiplayer implementation, score management, and trajectory prediction had very unique problems to solve. The amount of work and attention to detail required gave me a newfound appreciation for the games I play in my free time."
+        text: "At the point of starting this project, I had already been working with Unity for a few years. However, I had only made small prototypes which amounted to nothing substantial. This project challenged me to commit myself to a game and see it through to the end, so I had to learn a lot about polishing a game for release. Playtesting, for example, was integral to getting things right. There were also challenges that arose from the type of game I was making. For example, multiplayer implementation, score management, and trajectory prediction had very unique problems to solve."
       },
       {
         title: "What challenges did I face?",
-        text: "Because this was a solo project, I had to figure out everything by myself. Any problem I had involved a lot of research and rubber ducks, but making it through to the end was very empowering, knowing I did it all without someone guiding me through it."
+        text: "Because this was a solo project, I had to figure out everything by myself, with a lot of research and rubber ducks. Making it through to the end was very empowering, knowing I did it all without someone guiding me through it. The amount of work and attention to detail required gave me a newfound appreciation for the many games I've played."
       },
       {
         title: "What's next?",
-        text: "I've always had a passion for video games, so having made my own is a childhood dream come true. Nowadays when playing games, I'm able to appreciate cleverly designed mechanics, and I find it fun to try and reverse engineer them. I have plenty of ideas in mind for games I could make in the future, and I look forward to the day that I can execute on them. The satisfaction of seeing other people find fun in something I made is a unbeatable feeling that I can't wait to experience again."
+        text: "Having made my own video game is a childhood dream come true. Nowadays, I find it fun to try and reverse engineer the games I play in my free time. I have plenty of ideas in mind for games I could make in the future, and I look forward to the day that I can execute on them. The satisfaction of seeing other people find fun in something I made is an unbeatable feeling that I can't wait to experience again."
       }
     ]
   },
