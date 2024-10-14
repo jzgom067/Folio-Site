37cff2023bc1c1ef4e98c2aae986ef8971ebe752
jzgom067 <jzgombic3@gmail.com>
2024-10-13 23:18:35 -0400

    Add reactivity to new project layout
    It condenses to one column when the display gets narrow, but a bit of fixing is needed.

--- folio-app/src/components/Project.js
+++ folio-app/src/components/Project.js
@@ -10,7 +10,7 @@ import PlaceholderImage from './PlaceholderImage.js';
 import HomeButton from './HomeButton.js';
 
 // react stuff
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // router
 import { useParams } from 'react-router-dom';
@@ -25,6 +25,25 @@ function getProj(slug) {
 function Project() {
   let { proj } = useParams();
   proj = getProj(proj);
+
+  const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+  console.log(rem);
+
+  function checkSingleColumn() {
+    return window.innerWidth <= 55 * rem;
+  }
+
+  const [isSingleColumn, setsingleColumn] = useState(checkSingleColumn());
+
+  const updateVariable = () => {
+    // don't need to check if the value is changed, since React won't rerender unless
+    // the value has changed
+    setsingleColumn(checkSingleColumn());
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateVariable);
+  })
 
   return (
     <div className={styles.section}>
@@ -78,12 +97,30 @@ function Project() {
           </SlideFadeIn>
         </div>
       </div>
-      <div className={styles.content}>
-        <div className={styles.leftCol}>
+      {isSingleColumn &&
+        <div className={styles.singleCol}>
+          <div className={styles.previewContainer}>
+            <SlideFadeIn>
+              <div className={styles.previewHover}>
+                <span>View Media &#40;{proj.images.length}&#41;</span>
+              </div>
+              <ImageModal images={proj.images} />
+              <PlaceholderImage
+                className={styles.preview2}
+                src={proj.images[1].img}
+                alt={proj.images[1].title}
+                aspectRatio="16 / 9"
+              />
+              <PlaceholderImage
+                className={styles.preview}
+                src={proj.images[0].img}
+                alt={proj.images[0].title}
+                aspectRatio="16 / 9"
+                tempStyle={{ left: "5%" }}
+              />
+            </SlideFadeIn>
+          </div>
           {proj.text.map((dict, i) => {
-            if (i > proj.text.length / 2) {
-              return <></>;
-            }
             return (
               <div className={styles.textSection} key={i}>
                 <SlideFadeIn>
@@ -98,49 +135,72 @@ function Project() {
             );
           })}
         </div>
-        <div className={styles.rightCol}>
-          {proj.images.length > 0 &&
-            <div className={styles.previewContainer}>
-              <SlideFadeIn>
-                <div className={styles.previewHover}>
-                  <span>View Media &#40;{proj.images.length}&#41;</span>
+      }
+      {!isSingleColumn &&
+        <div className={styles.content}>
+          <div className={styles.leftCol}>
+            {proj.text.map((dict, i) => {
+              if (i > proj.text.length / 2) {
+                return <></>;
+              }
+              return (
+                <div className={styles.textSection} key={i}>
+                  <SlideFadeIn>
+                    <h2>{dict.title}</h2>
+                  </SlideFadeIn>
+                  <SlideFadeIn>
+                    <div className={styles.text}>
+                      {dict.text}
+                    </div>
+                  </SlideFadeIn>
                 </div>
-                <ImageModal images={proj.images} />
-                <PlaceholderImage
-                  className={styles.preview2}
-                  src={proj.images[1].img}
-                  alt={proj.images[1].title}
-                  aspectRatio="16 / 9"
-                />
-                <PlaceholderImage
-                  className={styles.preview}
-                  src={proj.images[0].img}
-                  alt={proj.images[0].title}
-                  aspectRatio="16 / 9"
-                  tempStyle={{ left: "5%" }}
-                />
-              </SlideFadeIn>
-            </div>
-          }
-          {proj.text.map((dict, i) => {
-            if (i <= proj.text.length / 2) {
-              return <></>;
-            }
-            return (
-              <div className={styles.textSection} key={i}>
+              );
+            })}
+          </div>
+          <div className={styles.rightCol}>
+            {proj.images.length > 0 &&
+              <div className={styles.previewContainer}>
                 <SlideFadeIn>
-                  <h2>{dict.title}</h2>
-                </SlideFadeIn>
-                <SlideFadeIn>
-                  <div className={styles.text}>
-                    {dict.text}
+                  <div className={styles.previewHover}>
+                    <span>View Media &#40;{proj.images.length}&#41;</span>
                   </div>
+                  <ImageModal images={proj.images} />
+                  <PlaceholderImage
+                    className={styles.preview2}
+                    src={proj.images[1].img}
+                    alt={proj.images[1].title}
+                    aspectRatio="16 / 9"
+                  />
+                  <PlaceholderImage
+                    className={styles.preview}
+                    src={proj.images[0].img}
+                    alt={proj.images[0].title}
+                    aspectRatio="16 / 9"
+                    tempStyle={{ left: "5%" }}
+                  />
                 </SlideFadeIn>
               </div>
-            );
-          })}
+            }
+            {proj.text.map((dict, i) => {
+              if (i <= proj.text.length / 2) {
+                return <></>;
+              }
+              return (
+                <div className={styles.textSection} key={i}>
+                  <SlideFadeIn>
+                    <h2>{dict.title}</h2>
+                  </SlideFadeIn>
+                  <SlideFadeIn>
+                    <div className={styles.text}>
+                      {dict.text}
+                    </div>
+                  </SlideFadeIn>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      }
       <SlideFadeIn>
         <HomeButton />
       </SlideFadeIn>
