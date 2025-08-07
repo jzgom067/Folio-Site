ccb5838fdb09ba6655c5274f71203a20b2aec735
jzgom067 <jzgombic3@gmail.com>
2025-08-06 22:06:30 -0400

    Add proper format to commit tiles
    Now it's getting closer to what I envisioned.

--- folio-app/src/components/project_page/Commit.js
+++ folio-app/src/components/project_page/Commit.js
@@ -4,18 +4,32 @@ import PlaceholderImage from '../common/PlaceholderImage.js';
 // styling
 import styles from './Commit.module.css';
 
-const GitHubCommit = ({ data }) => {
-// Get only the first line of the commit message
-const firstLine = data.commit.message.split('\n')[0];
+const Commit = ({ data }) => {
+    // Get only the first line of the commit message
+    const summary = data.commit.message.split('\n')[0];
+    const message = data.commit.message.split('\n').slice(1).join('\n');
 
-return (
-    <div className={styles.commit}>
-        {firstLine}
-        <div>
-            {data.commit.author.name} - {new Date(data.commit.author.date).toLocaleString()}
+    return (
+        <div className={styles.commit}>
+            <PlaceholderImage
+                className={styles.avatar}
+                src={data.author.avatar_url}
+                alt={data.author.login}
+                aspectRatio="1"
+            />
+            <div className={styles.text}>
+                <div className={styles.titleLine}>
+                    <div className={styles.summary}>
+                        {summary}
+                    </div>
+                    <div className={styles.author}>
+                        {data.author?.login || data.commit.author.name}, {new Date(data.commit.author.date).toLocaleDateString()}
+                    </div>
+                </div>
+                {message && <div className={styles.message}>{message}</div>}
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
-export default GitHubCommit;
+export default Commit;
