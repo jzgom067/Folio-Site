4b8eb2011ab38325de4a115d37ab9c105aad0fb9
jzgom067 <jzgombic3@gmail.com>
2026-04-15 14:18:29 -0400

    Add play icon

--- folio-app/src/components/common/Icon.js
+++ folio-app/src/components/common/Icon.js
@@ -197,6 +197,12 @@ const icons = {
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
     </svg>
   ),
+  "Play": (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-circle" viewBox="-1 -1 18 18">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+      <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+    </svg>
+  ),
   // Source: https://streamlinehq.com
   "Drizzle ORM": (
     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="Drizzle--Streamline-Simple-Icons">
